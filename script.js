1fcd606750ea3fbdda413e06867fa7e75ee2f6eb
El-Yafie <144225191+El-Yafie@users.noreply.github.com>
2026-02-21 21:36:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -98,7 +98,7 @@ function showSlides() {
     slideIndex++;
     if (slideIndex > slides.length) { slideIndex = 1 }
     slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 3000); // Change image every 3 seconds
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
 
 // --- Music ---
@@ -182,3 +182,4 @@ function fireConfetti() {
         startVelocity: 45,
     });
 }
+
